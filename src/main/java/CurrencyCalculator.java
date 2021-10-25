ed04c49d9d3587d1218b92f380d7de06b715baef
Leah <leah@uxwebtech.com>
2021-10-25 11:55:11 +0300

    Added main

--- src/main/java/CurrencyCalculator.java
+++ src/main/java/CurrencyCalculator.java
@@ -6,50 +6,54 @@ public class CurrencyCalculator {
     private static int input;
     private static double amount;
     private static double rate;
+    private static String startOver = "Y";
 
     public static void main(String[] args){
-
-        FirstScreen();
-//If if the input was correct and get the rate
-        switch(input){
-            case 1:
+        //Only show once
+        System.out.println("Welcome to currency converter");
+        while (startOver == "Y" || startOver == "y") {
+            FirstScreen();
+        //If the input was correct and get the rate
+            switch (input) {
+                case 1:
+                    //USD
+                    Coin USDcurrency = new USD();
+                    rate = USDcurrency.getValue();
+                    break;
+                case 2:
+                    //ILS
+                    Coin ILScurrency = new ILS();
+                    rate = ILScurrency.getValue();
+                    break;
+                default:
+                    //in case niether 1 or 2 was the input
+                    System.out.println("The invalid input was entered");
+                    FirstScreen();
+            }
+        //Based on the rate calculate the conversion total
+            if (rate == 3.52) {
                 //USD
-                Coin USDcurrency = new USD();
-                rate = USDcurrency.getValue();
-                break;
-            case 2:
+                SecondScreen();
+                USD total = new USD();
+                ThirdScreen();
+                System.out.println(total.calculate(amount));
+                System.out.println(StartOver());
+            } else if (rate == 0.28) {
+
                 //ILS
-                Coin ILScurrency = new ILS();
-                rate = ILScurrency.getValue();
-                break;
-            default:
-                //in case niether 1 or 2 was the input
-                System.out.println("The invalid input was entered");
-                FirstScreen();
+                SecondScreen();
+                ILS total = new ILS();
+                ThirdScreen();
+                System.out.println(total.calculate(amount));
+                System.out.println(StartOver());
+            } else System.out.println("Start over!");
         }
-//Based on the rate calculate the conversion total
-        if (rate == 3.52) {
-            //USD
-            SecondScreen();
-            USD total = new USD();
-            ThirdScreen();
-            System.out.println(total.calculate(amount));
-        }
-        else if (rate == 0.28){
-
-            //ILS
-            SecondScreen();
-            ILS total = new ILS();
-            ThirdScreen();
-            System.out.println(total.calculate(amount));
-        }
-        else System.out.println("Start over!");
-
+        if (startOver != "Y" || startOver != "y")
+        FourthScreen();
 
     }
     //Welcome Screen with input of currency choice
     public static Integer FirstScreen(){
-        System.out.println("Welcome to currency converter");
         System.out.println("Please choose an option (1/2):");
         Scanner scanner = new Scanner(System.in);
         input = scanner.nextInt();
@@ -65,5 +69,15 @@ public class CurrencyCalculator {
     //Third screen with conversion calculation
     public static void ThirdScreen(){
         System.out.println("The converted amount is: " );
+    }
+    //Start Over screen
+    public static String StartOver(){
+        System.out.println("Do you want to calculate another conversion? (Y/N)");
+        Scanner scanner = new Scanner(System.in);
+        startOver = scanner.next();
+        return startOver;
+    }
+    public static void FourthScreen(){
+        System.out.println("Thanks for using our currency converter");
     }
 }
